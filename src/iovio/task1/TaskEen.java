a0ab2b6db6f3748119e69fc4d560f49b8a3818bc
jctjavier <joannecelina.javier@gmail.com>
2019-08-15 12:45:33 +0200

    Task1 : Line Reduction and Comments

--- src/iovio/task1/TaskEen.java
+++ src/iovio/task1/TaskEen.java
@@ -8,27 +8,22 @@ import java.util.*;
  */
 public class TaskEen {
 	public static void main(String[] args) {
-		boolean tallyHo = false;
-		Random rand = new Random();
+		Random rand = new Random(); // Use random util to generate random int
 
-		int lambInt = rand.nextInt(9);
-		int sigInt = rand.nextInt(9);
-		int gamInt = rand.nextInt(9);
-		int lalalaInt = Integer.parseInt(Integer.toString(lambInt) + Integer.toString(lambInt) + Integer.toString(lambInt));
+		int lambInt = rand.nextInt(9); // Generate random int for lambda (from 0 to 9)
+		int sigInt = rand.nextInt(9); // Generate random int for sigma (from 0 to 9)
+		int gamInt = rand.nextInt(9); // Generate random int for gamma (from 0 to 9)
+		int lalalaInt = Integer.parseInt(Integer.toString(lambInt) + Integer.toString(lambInt) + Integer.toString(lambInt)); // Generate lambdalambdalamda integer by first concatenating lambda (that were converted to string)
 
-		String lambdaSigma = Integer.toString(lambInt) + Integer.toString(sigInt);
+		String lambdaSigma = Integer.toString(lambInt) + Integer.toString(sigInt); // Formula requires a digit that combines the generate number from lambda and sigma. This string will be this digit later on.
 
-		if (lalalaInt == (Integer.parseInt(lambdaSigma) * gamInt)) {
-			tallyHo = true;
-		}
-
-		if (tallyHo) {
-			System.out.println("lambda = " + lambInt);
+		if (lalalaInt == (Integer.parseInt(lambdaSigma) * gamInt)) { // If condition checks if the lambdalambdalambda integer we have generated is equal to lambdasigma multiplied by gamma
+			System.out.println("lambda = " + lambInt); // If condition is true, it displays the lambda, sigma, and gamma digits used
 			System.out.println("sigma = " + sigInt);
 			System.out.println("gamma = " + gamInt);
 		} else {
 			System.out.println(lambdaSigma + " x " + gamInt + " <=> " + (Integer.parseInt(lambdaSigma) * gamInt) + " = "
-					+ Integer.toString(lambInt) + Integer.toString(lambInt) + Integer.toString(lambInt));
+					+ Integer.toString(lambInt) + Integer.toString(lambInt) + Integer.toString(lambInt)); // If condition is false, it displays the inequality based on task specifications
 		}
 	}
 }
